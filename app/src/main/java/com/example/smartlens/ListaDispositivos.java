6eb6ed2b14c91070ed106afb41aed818cf7c1f7a
FerCJ <carlos43472@gmail.com>
2021-04-26 18:10:56 -0500

    Se añade la funcionalidad de mostar todos los dispositivos con los que se ha empearejado el celular

---
+++ app/src/main/java/com/example/smartlens/ListaDispositivos.java
@@ -0,0 +1,115 @@
+package com.example.smartlens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class ListaDispositivos extends AppCompatActivity {
+
+    private ListView listaEmparejados, listaDisponibles;
+    private ArrayAdapter<String> adaptadorDisponibles, adaptadorEmparejados;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private View vista;
+    private ProgressBar progressBar;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_dispositivos);
+        context = this;
+
+        inicializar();
+
+     /*   listaEmparejados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finishAffinity();
+
+                // Realiza un intent para iniciar la siguiente actividad
+                // mientras toma un EXTRA_DEVICE_ADDRESS que es la dirección MAC.
+                Intent intend = new Intent(ListaDispositivos.this, MainActivity.class);
+                intend.putExtra("deviceAddress", address);
+                startActivity(intend);
+
+            }
+        });*/
+
+    }
+
+    private BroadcastReceiver bluetoothListener = new BroadcastReceiver() {
+        @Override
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED)
+                {
+                    //adaptadorDisponibles.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+
+            }
+        }
+    };
+
+    // Configura un (on-click) para la lista
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            // Obtener la dirección MAC del dispositivo, que son los últimos 17 caracteres en la vista
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+
+        }
+    };
+
+    public void inicializar()
+    {
+        listaEmparejados = findViewById(R.id.lvEmparejados);
+
+        adaptadorEmparejados = new ArrayAdapter<String>(this, R.layout.lista_dispositivos_item);
+
+        listaEmparejados.setAdapter(adaptadorEmparejados);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> emparejados = bluetoothAdapter.getBondedDevices();
+
+        if (emparejados != null && emparejados.size()>0)
+        {
+            for (BluetoothDevice device : emparejados)
+            {
+                adaptadorEmparejados.add(device.getName() + '\n' + device.getAddress());
+            }
+        }
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bluetoothListener,intentFilter);
+        IntentFilter intentFilter2 = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bluetoothListener,intentFilter2);
+
+    }
+}
--- app/src/main/java/com/example/smartlens/MainActivity.java
+++ app/src/main/java/com/example/smartlens/MainActivity.java
@@ -1,44 +1,235 @@
 package com.example.smartlens;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
+
+    private BTUtils btUtils;
+
+    private final int LOCATION_PERMISSION_REQUEST = 101;
+    private final int SELECT_DEVICE = 102;
+
+    public static final int MESSAGE_STATE_CHANGED = 0;
+    public static final int MESSAGE_READ = 1;
+    public static final int MESSAGE_WRITE = 2;
+    public static final int MESSAGE_DEVICE_NAME = 3;
+    public static final int MESSAGE_TOAST = 4;
+
+    public static final String DEVICE_NAME = "deviceName";
+    public static final String TOAST = "toast";
+    private String connectedDevice;
+
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+
+            switch (message.what)
+            {
+                case MESSAGE_STATE_CHANGED:
+                    switch (message.arg1)
+                    {
+                        case BTUtils.STATE_NONE:
+                            btUtils.setState(0);
+                            break;
+                        case BTUtils.STATE_LISTEN:
+                            btUtils.setState(1);
+                            break;
+                        case BTUtils.STATE_CONNECTING:
+                            btUtils.setState(2);
+                            break;
+                        case BTUtils.STATE_CONNECTED:
+                            btUtils.setState(3);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case MESSAGE_READ:
+                    break;
+                case MESSAGE_WRITE:
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    connectedDevice = message.getData().getString(DEVICE_NAME);
+                    Toast.makeText(context, connectedDevice,Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(context, message.getData().getString(TOAST),Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+
+            return false;
+        }
+    });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context=this;
-        initBluetooth();
+
+        inicialziarBT();
+
+        btUtils = new BTUtils(context, handler);
+
+        ImageButton bluetooth = (ImageButton) findViewById(R.id.imageButton5);
+        bluetooth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                encenderBT();
+                revisarPermisos();
+
+            }
+        });
+
+        btUtils = new BTUtils(context,handler);
     }
 
-    private void initBluetooth() {
+    private void inicialziarBT()
+    {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            //SI SALE JEFEEEEEE
-            Toast.makeText(context, "El dispositivo no soporta conexion a Bluetooth", Toast.LENGTH_SHORT).show();
+
+        if (bluetoothAdapter == null)
+        {
+            /*Snackbar.make(view, "Click detectado", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();*/
+            Toast.makeText(context, "Bluetooth no encontrado",Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void enableBluetooth() {
+    private  void  encenderBT()
+    {
         if (bluetoothAdapter.isEnabled())
-        {Toast.makeText(context, "El Bluetooth está Encendido", Toast.LENGTH_SHORT).show();
-        } else {
+        {
+            Toast.makeText(context, "Bluetooth ya esta encendido",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
             bluetoothAdapter.enable();
         }
+
+        if (bluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE)
+        {
+            Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
+            startActivity(discoveryIntent);
+        }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (btUtils != null)
+        {
+            btUtils.stop();
+        }
+    }
+
+    private void  revisarPermisos()
+    {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+
+        }
+        else
+        {
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                }
+            }, 3000);
+            Intent intent = new Intent(this,ListaDispositivos.class);
+            startActivityForResult(intent,SELECT_DEVICE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == SELECT_DEVICE &&  resultCode == RESULT_OK)
+        {
+            String address = data.getStringExtra("deviceAddress");
+            btUtils.connect2(bluetoothAdapter.getRemoteDevice(address));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+
+            }
+            else
+            {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Otorgar los permisos es requerido")
+                        .setPositiveButton("Permitir", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                revisarPermisos();
+                            }
+                        })
+                        .setNegativeButton("Rechazar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .create();
+            }
+        }
+        else
+        {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
 
     //Método para el botón Reloj
     public void MensajeReloj(View view){
         Toast.makeText(this, "Esto es un mensaje para el Reloj", Toast.LENGTH_SHORT) .show();
+
     }
 
     //Método para el botón Calendario
@@ -58,8 +249,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Método para el botón BT
     public boolean MensajeBT(View view){
-        //Toast.makeText(this, "Esto es un mensaje para el BT", Toast.LENGTH_SHORT) .show();
-        enableBluetooth();
+        Toast.makeText(this, "Esto es un mensaje para el BT", Toast.LENGTH_SHORT) .show();
         return true;
     }
 
@@ -67,5 +257,7 @@ public class MainActivity extends AppCompatActivity {
     public void MensajeInfo(View view){
         Toast.makeText(this, "Esto es un mensaje para Información", Toast.LENGTH_SHORT) .show();
     }
+
+
 
 }
